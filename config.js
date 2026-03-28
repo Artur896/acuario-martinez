579bebb85f4cc2e4570b88d0e855cd1f4f6febd2
Artur896 <delacruzarturo896@gmail.com>
2026-03-28 13:59:58 -0600

    Refactor: Mejora del diseño en el frontend

--- config.js
+++ config.js
@@ -8,4 +8,4 @@ const SUPABASE_URL = 'https://xaribdaqtlogluwszfsv.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_JLk_vqpODK3JdzYCL-OaPA_h54Kk52H';
 
 // WhatsApp number for order notifications (format: 521 + 10 digits)
-const WHATSAPP_NUMBER = '5217296463642';
+const WHATSAPP_NUMBER = '527296440441';
